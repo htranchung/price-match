11e37766fcbfb145dd89441f88f74db743a9fbe5
Harrison Tran-Chung <tranchung@live.com>
2023-05-18 21:19:34 -0500

    linked buttons in navbar and hero section to go to location

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import LogoOnly from '../assets/LogoOnly.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
-                    <a href="#" className="flex items-center">
+                    <Link to="/" className="flex items-center">
                         <img src={LogoOnly} className='iconOnly' alt='Pricema AI Icon' />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Pricema AI</span>
-                    </a>
+                    </Link>
                     <div className="flex md:order-2">
-                        <button type="button" className="spacingButton text-black bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 text-center mr-3  md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button>
-                        <button type="button" className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button>
+                        <Link to="/Login">
+                            <button href='#loginPage' type="button" className="spacingButton text-black bg-white hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 text-center mr-3  md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign In</button>
+                        </Link>
+                        <Link to="/Signup">
+                            <button type="button" className="text-white bg-black hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button>
+                        </Link>
                     </div>
                     <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
