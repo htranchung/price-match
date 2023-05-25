b5661bd1873bcdfa486a5323c8952044359c858d
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-25 16:43:24 -0500

    Added new visual for Howitworks section

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Lottie from "lottie-react";
-import animationData from "../assets/Ai-Search.json";
+import animationData from "../assets/Data-Analysis.json";
 import animationData2 from "../assets/Ai-Chat.json";
 import animationData3 from "../assets/AI-Analysis.json";
 import { Link } from "react-router-dom";
@@ -26,12 +26,11 @@ const HowItWorks = () => {
 
           <div className="simple-search-card flex flex-wrap -m-4">
             <div className="card-info-section p-4 lg:w-1/2">
-              <h2 className="card-heading">Visualization Your Data</h2>
+              <h2 className="card-heading">Visualize your Data</h2>
               <p className="card-text">
                 Upload your data into Pricema's data visualization tool and fine
                 tune your data. When you are done simply download your updated
-                data set and take it to it to Pricema chat to talk with your
-                data.
+                data set and take it to Pricema chat to talk with your data.
               </p>
               <a
                 href="https://tanvirkhon-xcel-editor-app-gih7s1.streamlit.app/
@@ -75,11 +74,11 @@ const HowItWorks = () => {
               <div>
                 <h2 className="analysis-heading">Pricema Analysis</h2>
                 <p className="analysis-text">
-                  Ready to take you data analysis to the next level?Our advanced
-                  AI analysis technology leverages cutting-edge algorithms to
-                  process and extract valuable insights from complex data sets,
-                  empowering you to make data-driven decisions with speed and
-                  accuracy.
+                  Ready to take you data analysis to the next level? Our
+                  advanced AI analysis technology leverages cutting-edge
+                  algorithms to process and extract valuable insights from
+                  complex data sets, empowering you to make data-driven
+                  decisions with speed and accuracy.
                 </p>
                 <Link to="/Payment">
                   <button className="ai-analysis-btn">AI Analysis</button>
